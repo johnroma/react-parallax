993947e2951c6c19e5354955851eda34ba6a4193
johnroma <login@john.ro>
2024-03-01 22:40:35 +0100

    animated clouds

--- src/components/ReactParallax.tsx
+++ src/components/ReactParallax.tsx
@@ -1,5 +1,5 @@
 import { useEffect, useRef } from 'react'
-import { useScroll, motion, useTransform } from 'framer-motion'
+import { useScroll, motion, useTransform, useAnimation } from 'framer-motion'
 import Lenis from '@studio-freight/lenis'
 import styles from './ReactParallax.module.scss'
 
@@ -8,22 +8,16 @@ const cloud = 'cloud1.png'
 
 export function ReactParallax() {
   const container = useRef(null)
+  const controls = useAnimation()
+
   const { scrollYProgress: scrollYgreen } = useScroll({
     target: container,
     offset: ['start start', 'end end'],
   })
 
-  // const { scrollYProgress: scrollYpink } = useScroll({
-  //   target: container,
-  //   // FROM target/window TO target/window
-  //   offset: ['start end', 'end end'],
-  // })
   const far = useTransform(scrollYgreen, [0, 1], ['0%', '-16%'])
   const closer = useTransform(scrollYgreen, [0, 1], ['0%', '-300%'])
 
-  // useMotionValueEvent(scrollYgreen, 'change', (latest) => {
-  //   console.log('Page scroll: ', latest)
-  // })
   useEffect(() => {
     const lenis = new Lenis()
 
@@ -33,17 +27,38 @@ export function ReactParallax() {
     }
 
     requestAnimationFrame(raf)
+
+    return () => {
+      lenis.destroy() // Cleanup Lenis instance on unmount
+    }
   }, [])
+
+  useEffect(() => {
+    let isCancelled = false
+
+    async function sequence() {
+      if (!isCancelled) {
+        await controls.start({ x: '0vw' }, { duration: 10, ease: 'linear' })
+        await controls.start({ x: '-50vw' }, { duration: 10, ease: 'linear' })
+        await controls.start({ x: '50vw' }, { duration: 0, delay: 4 })
+        await controls.start({ x: '0vw' }, { duration: 10, ease: 'linear' })
+        sequence() // Loop animation
+      }
+    }
+
+    sequence()
+
+    return () => {
+      isCancelled = true // Set flag to cancel the animation sequence
+    }
+  }, [controls])
+
   return (
     <>
       <motion.div
         className={styles.progressBar}
         style={{ backgroundColor: 'green', opacity: 0, scaleX: scrollYgreen }}
       />
-      {/* <motion.div
-        className={styles.progressBar}
-        style={{ backgroundColor: 'pink', opacity: 1, top: 10, scaleX: scrollYpink }}
-      /> */}
 
       <div ref={container} className={styles.container}>
         <img alt="city" width="128" height="407" className={styles.closest} src="city-16bit-sm.png" />
@@ -53,13 +68,14 @@ export function ReactParallax() {
             <img width="128" height="407" className={styles.farbg} alt="bg" src={bg} />
           </motion.div>
           <motion.div className={styles.el} style={{ y: closer }}>
-            <img
+            <motion.img
+              initial={{ scale: 2.5 }}
               alt="cloud"
-              style={{ paddingBottom: '-20%', transform: 'scale(2.5)' }}
               height="34"
               width="70"
               className={styles.closer}
               src={cloud}
+              animate={controls}
             />
           </motion.div>
         </div>
