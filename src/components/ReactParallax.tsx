dc23d09e198637fab35bc6d5071ba8a8257ebedc
johnroma <login@john.ro>
2024-03-01 02:32:47 +0100

    mvp

--- src/components/ReactParallax.tsx
+++ src/components/ReactParallax.tsx
@@ -1,5 +1,6 @@
-import { useRef } from 'react'
-import { useScroll, useMotionValueEvent, motion, useTransform } from 'framer-motion'
+import { useEffect, useRef } from 'react'
+import { useScroll, motion, useTransform } from 'framer-motion'
+import Lenis from '@studio-freight/lenis'
 import styles from './ReactParallax.module.scss'
 
 const bg = 'city-16bit-bg.png'
@@ -7,33 +8,54 @@ const cloud = 'cloud1.png'
 
 export function ReactParallax() {
   const container = useRef(null)
-  const { scrollYProgress } = useScroll({
+  const { scrollYProgress: scrollYgreen } = useScroll({
     target: container,
     offset: ['start start', 'end end'],
   })
 
-  const far = useTransform(scrollYProgress, [0, 1], [0, -180])
-  const closer = useTransform(scrollYProgress, [0, 1], [0, -100])
+  // const { scrollYProgress: scrollYpink } = useScroll({
+  //   target: container,
+  //   // FROM target/window TO target/window
+  //   offset: ['start end', 'end end'],
+  // })
+  const far = useTransform(scrollYgreen, [0, 1], ['0%', '-16%'])
+  const closer = useTransform(scrollYgreen, [0, 1], ['0%', '-300%'])
 
-  useMotionValueEvent(scrollYProgress, 'change', (latest) => {
-    console.log('Page scroll: ', latest)
-  })
+  // useMotionValueEvent(scrollYgreen, 'change', (latest) => {
+  //   console.log('Page scroll: ', latest)
+  // })
+  useEffect(() => {
+    const lenis = new Lenis()
+
+    function raf(time: number) {
+      lenis.raf(time)
+      requestAnimationFrame(raf)
+    }
+
+    requestAnimationFrame(raf)
+  }, [])
   return (
     <>
-      <motion.div className={styles.progressBar} style={{ backgroundColor: 'green', scaleX: scrollYProgress }} />
-      <h1>ReactParallax</h1>
+      <motion.div
+        className={styles.progressBar}
+        style={{ backgroundColor: 'green', opacity: 0, scaleX: scrollYgreen }}
+      />
+      {/* <motion.div
+        className={styles.progressBar}
+        style={{ backgroundColor: 'pink', opacity: 1, top: 10, scaleX: scrollYpink }}
+      /> */}
 
       <div ref={container} className={styles.container}>
         <img alt="city" width="128" height="407" className={styles.closest} src="city-16bit-sm.png" />
 
         <div className={styles.sticky}>
-          <motion.div className={styles.el} style={{ y: far }}>
+          <motion.div className={styles.bgel} style={{ y: far }}>
             <img width="128" height="407" className={styles.farbg} alt="bg" src={bg} />
           </motion.div>
           <motion.div className={styles.el} style={{ y: closer }}>
             <img
               alt="cloud"
-              style={{ paddingTop: '10%' }}
+              style={{ paddingBottom: '-20%', transform: 'scale(2.5)' }}
               height="34"
               width="70"
               className={styles.closer}
