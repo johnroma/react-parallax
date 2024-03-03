e56c8e6f0bf7458d4dc91e30a81a6c875c160311
johnroma <login@john.ro>
2024-03-03 01:58:43 +0100

    scaling cloud

--- src/components/Parallax.tsx
+++ src/components/Parallax.tsx
@@ -1,9 +1,9 @@
-import { useEffect, useRef } from 'react'
+import { useEffect, useRef, useState } from 'react'
 import { useScroll, motion, useTransform, useAnimation } from 'framer-motion'
 import Lenis from '@studio-freight/lenis'
 import styles from './Parallax.module.css'
 
-export function Parallax({ bg, cloud, foreground }: { bg: string; cloud: string; foreground: string }) {
+export function Parallax({ bg, cloud, foreground }: Readonly<{ bg: string; cloud: string; foreground: string }>) {
   const container = useRef(null)
   const controls = useAnimation()
 
@@ -30,6 +30,45 @@ export function Parallax({ bg, cloud, foreground }: { bg: string; cloud: string;
     }
   }, [])
 
+  const imgRef = useRef<HTMLImageElement>(null)
+  const [dimensions, setDimensions] = useState({
+    originalWidth: 0,
+    originalHeight: 0,
+    renderedWidth: 0,
+    renderedHeight: 0,
+  })
+
+  const handleImageLoad = () => {
+    if (imgRef.current) {
+      setDimensions({
+        originalWidth: imgRef.current.naturalWidth,
+        originalHeight: imgRef.current.naturalHeight,
+        renderedWidth: imgRef.current.offsetWidth,
+        renderedHeight: imgRef.current.offsetHeight,
+      })
+    }
+  }
+
+  useEffect(() => {
+    const handleResize = () => {
+      if (!imgRef.current) return
+      setDimensions({
+        ...dimensions,
+        renderedWidth: imgRef.current.offsetWidth,
+        renderedHeight: imgRef.current.offsetHeight,
+      })
+    }
+
+    window.addEventListener('resize', handleResize)
+
+    // Clean up the event listener when the component unmounts
+    return () => {
+      window.removeEventListener('resize', handleResize)
+    }
+  }, [dimensions])
+
+  const scaling = dimensions.renderedWidth / dimensions.originalWidth
+
   useEffect(() => {
     let isCancelled = false
 
@@ -39,6 +78,26 @@ export function Parallax({ bg, cloud, foreground }: { bg: string; cloud: string;
         await controls.start({ x: '-50vw' }, { duration: 10, ease: 'linear' })
         await controls.start({ x: '50vw' }, { duration: 0, delay: 4 })
         await controls.start({ x: '0vw' }, { duration: 10, ease: 'linear' })
+        // sequence() // Loop animation
+      }
+    }
+
+    sequence()
+
+    return () => {
+      isCancelled = true // Set flag to cancel the animation sequence
+    }
+  }, [controls])
+
+  useEffect(() => {
+    let isCancelled = false
+
+    async function sequence() {
+      if (!isCancelled) {
+        await controls.start({ x: '0vw' }, { duration: 30, ease: 'linear' })
+        await controls.start({ x: '-100vw' }, { duration: 30, ease: 'linear' })
+        await controls.start({ x: '100vw' }, { duration: 0, delay: 2 })
+        await controls.start({ x: '0vw' }, { duration: 30, ease: 'linear' })
         sequence() // Loop animation
       }
     }
@@ -52,7 +111,7 @@ export function Parallax({ bg, cloud, foreground }: { bg: string; cloud: string;
 
   return (
     <div ref={container} className={styles.container}>
-      <div>{styles.progressBar}bs</div>
+      <div className={styles.progressBar}>{scaling}</div>
       <motion.div
         className={styles.progressBar}
         style={{ backgroundColor: 'green', opacity: 0, scaleX: scrollYgreen }}
@@ -60,17 +119,23 @@ export function Parallax({ bg, cloud, foreground }: { bg: string; cloud: string;
       <img alt="city" width="128" height="407" className={styles.closest} src={foreground} />
       <div className={styles.sticky}>
         <motion.div className={styles.bgel} style={{ y: far }}>
-          <img width="128" height="407" className={styles.farbg} alt="bg" src={bg} />
+          <img
+            width="128"
+            height="407"
+            className={styles.farbg}
+            alt="bg"
+            src={bg}
+            onLoad={handleImageLoad}
+            ref={imgRef}
+          />
         </motion.div>
         <motion.div className={styles.el} style={{ y: closer }}>
           <motion.img
-            initial={{ scale: 2.5 }}
             alt="cloud"
-            height="34"
-            width="70"
             className={styles.closer}
             src={cloud}
             animate={controls}
+            style={{ width: `${scaling * 5}rem` }}
           />
         </motion.div>
       </div>
